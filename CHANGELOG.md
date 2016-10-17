90e7e28e1f8857e9572262159e96f895a537d5b2
zeng zhihui <zengzhihui@score.cool>
2016-10-17 20:02:35 +0800

    init

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+## fork SDWenbImage 3.8.2
+
+#### Features
+
+carthage build surppot webp framework
---
+++ SDWebImageWebp/SDWebImageWebp.h
@@ -0,0 +1,36 @@
+//
+//  SDWebImageWebp.h
+//  SDWebImageWebp
+//
+//  Created by zzh on 16/10/17.
+//  Copyright © 2016年 Gavin Zeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SDWebImageWebp.
+FOUNDATION_EXPORT double SDWebImageWebpVersionNumber;
+
+//! Project version string for SDWebImageWebp.
+FOUNDATION_EXPORT const unsigned char SDWebImageWebpVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SDWebImageWebp/PublicHeader.h>
+
+
+#import <WebImageWebP/SDWebImageManager.h>
+#import <WebImageWebP/SDImageCache.h>
+#import <WebImageWebP/UIImageView+WebCache.h>
+#import <WebImageWebP/SDWebImageCompat.h>
+#import <WebImageWebP/UIImageView+HighlightedWebCache.h>
+#import <WebImageWebP/SDWebImageDownloaderOperation.h>
+#import <WebImageWebP/UIButton+WebCache.h>
+#import <WebImageWebP/SDWebImagePrefetcher.h>
+#import <WebImageWebP/UIView+WebCacheOperation.h>
+#import <WebImageWebP/UIImage+MultiFormat.h>
+#import <WebImageWebP/SDWebImageOperation.h>
+#import <WebImageWebP/SDWebImageDownloader.h>
+#import <WebImageWebP/MKAnnotationView+WebCache.h>
+#import <WebImageWebP/SDWebImageDecoder.h>
+#import <WebImageWebP/UIImage+WebP.h>
+#import <WebImageWebP/UIImage+GIF.h>
+#import <WebImageWebP/NSData+ImageContentType.h>
